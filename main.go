4d9ad0e18dad98600a3673131a2171d421748a48
MaiAnnur <ebook.humaira@gmail.com>
2025-11-24 20:08:38 +0800

    feat:go http server

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type RequestData struct {
+	Text string `json:"text"`
+}
+
+func receiveJSONHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var data RequestData
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+
+	// Display in console
+	fmt.Printf("Received JSON: %+v\n", data)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":  "success",
+		"message": "JSON received",
+	})
+}
+
+func main() {
+	http.HandleFunc("/receive-json", receiveJSONHandler)
+
+	fmt.Println("Server starting on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
